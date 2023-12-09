ad6ba292550e856a32485d4280ec0acfeac7b918
Uzurch1k <galcenkovlad7@gmail.com>
2023-12-09 19:58:48 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,26 @@
 //====================================================================
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-// profile.changeUsername('Marco');
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+//====================================================================
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
